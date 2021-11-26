95d0de9f6fa8843da398e0f7346a7cf56e727268
Mikita Kukavenka <godinfrog@gmail.com>
2021-11-26 19:46:36 +0300

    init project

---
+++ src/main/java/by/c7d5a6/languageparser/LanguageParserApplication.java
@@ -0,0 +1,13 @@
+package by.c7d5a6.languageparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/c7d5a6/languageparser/LanguageParserApplicationTests.java
@@ -0,0 +1,13 @@
+package by.c7d5a6.languageparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
