6e5bd2912979a3a9a0d93d954462c04b1134ae7c
Mikita Kukavenka <godinfrog@gmail.com>
2024-08-30 12:29:14 +0300

    chnages to gradle 8.10

--- src/main/java/by/c7d5a6/languageparser/configuration/SecurityConfiguration.java
+++ src/main/java/by/c7d5a6/languageparser/configuration/SecurityConfiguration.java
@@ -1,15 +1,16 @@
 package by.c7d5a6.languageparser.configuration;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,8 +22,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfiguration {
 
 //    private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
 
@@ -42,8 +43,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         }
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http
 //                .authorizeRequests()
 //                .mvcMatchers("/docs/**").permitAll()
@@ -60,12 +61,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //                .oauth2ResourceServer(oauth2 -> oauth2
 //                        .authenticationManagerResolver(new JwtIssuerAuthenticationManagerResolver(oauthAuthenticationManager))
 //                )
-        http.oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthenticationConverter());;
-        http.csrf()
-                .disable()
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+        http.oauth2ResourceServer((oauth2ResourceServer) ->
+                oauth2ResourceServer.jwt((jwt) -> jwt
+                                        .jwtAuthenticationConverter(jwtAuthenticationConverter())));
+        http.csrf((csrf)-> {
+            try {
+                csrf.disable().cors(cors -> cors.configurationSource(corsConfigurationSource()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return http.build();
 
     }
 
