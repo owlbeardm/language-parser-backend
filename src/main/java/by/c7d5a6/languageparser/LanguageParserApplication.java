36f62688f43aa14e78a8007ea5e4f06a270de5fc
Mikita Kukavenka <nkukovenko@by.byelex.com>
2024-09-02 17:05:18 +0300

    finished updating

--- src/main/java/by/c7d5a6/languageparser/LanguageParserApplication.java
+++ src/main/java/by/c7d5a6/languageparser/LanguageParserApplication.java
@@ -8,36 +8,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.h2.tools.Server;
 
-import java.sql.SQLException;
 import java.time.Duration;
 
 @SpringBootApplication
 public class LanguageParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LanguageParserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageParserApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder
-				.setConnectTimeout(Duration.ofMillis(5000))
-				.setReadTimeout(Duration.ofMillis(5000))
-				.build();
-	}
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofMillis(5000))
+                .setReadTimeout(Duration.ofMillis(5000))
+                .build();
+    }
 
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper mapper = new ModelMapper();
-		mapper.getConfiguration()
-				.setMatchingStrategy(MatchingStrategies.STRICT)
-				.setFieldMatchingEnabled(true)
-				.setSkipNullEnabled(true)
-				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
-		return mapper;
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+        return mapper;
+    }
 
 //	@Bean(initMethod = "start", destroyMethod = "stop")
 //	public Server inMemoryH2DatabaseServer() throws SQLException {
